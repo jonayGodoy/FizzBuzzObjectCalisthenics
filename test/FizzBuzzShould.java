e06fccb29919bdbcc157c1efd73f829d90d67dd7
jonay <casa@gmail.com>
2017-10-21 03:48:04 +0100

    refactor test

--- test/FizzBuzzShould.java
+++ test/FizzBuzzShould.java
@@ -1,34 +1,46 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.*;
 
 public class FizzBuzzShould {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    private void assertThatPositionIs(int position,String s){
+        assertThat(fizzBuzz.returnPosition(position), is(new WrapperFizzBuzz(new WrapperString(s))));
+    }
+
     @Test
     public void testGreen() throws Exception {
         assertThat(true, is(true));
     }
     @Test
     public void first_position_them_1(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.returnPosition(1), is(new WrapperFizzBuzz(new WrapperString("1"))));
+        assertPositionIsCorrect();
+    }
+
+    private void assertPositionIsCorrect() {
+        assertThatPositionIs(1,"1");
     }
 
     @Test
     public void second_position_them_2() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.returnPosition(2), is(new WrapperFizzBuzz(new WrapperString("2"))));
+        assertThatPositionIs(2,"2");
     }
 
     @Test
     public void thirst_position_them_3() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.returnPosition(3), is(new WrapperFizzBuzz(new WrapperString("3"))));
+        assertThatPositionIs(3,"3");
     }
 
     @Test
     public void zero_position_them_empty() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        assertThat(fizzBuzz.returnPosition(0), is(new WrapperFizzBuzz(new WrapperString(""))));
+        assertThatPositionIs(0,"");
     }
-
 }
