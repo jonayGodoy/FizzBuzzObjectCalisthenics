4d858c75ead2107356223a48dacfd6e2a1e4c140
jonay <casa@gmail.com>
2017-10-21 02:54:05 +0100

    first test

---
+++ src/FizzBuzz.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Usuario on 21/10/2017.
+ */
+public class FizzBuzz {
+}
---
+++ test/FizzBuzzShould.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.*;
+
+public class FizzBuzzShould {
+    @Test
+    public void testGreen() throws Exception {
+        assertThat(true, is(true));
+    }
+}
