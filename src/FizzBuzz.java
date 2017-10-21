ff8df36d790971b1b95613540b45a0315580912d
jonay <casa@gmail.com>
2017-10-21 03:08:56 +0100

    green : second_position_them_2

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class FizzBuzz {
+
+    private List<WrapperFizzBuzz> list100;
+
+    public FizzBuzz() {
+        list100 = new ArrayList<>();
+        for(int i =0; i < 100;i++){
+            list100.add(i,new WrapperFizzBuzz(String.valueOf(i)));
+        }
+    }
+
+
     WrapperFizzBuzz returnPosition(int position) {
-        return new WrapperFizzBuzz("1");
+        return list100.get(position);
     }
 }
--- test/FizzBuzzShould.java
+++ test/FizzBuzzShould.java
@@ -13,4 +13,9 @@ public class FizzBuzzShould {
         assertThat(fizzBuzz.returnPosition(1), is(new WrapperFizzBuzz("1")));
     }
 
+    @Test
+    public void second_position_them_2() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.returnPosition(2), is(new WrapperFizzBuzz("2")));
+    }
 }
