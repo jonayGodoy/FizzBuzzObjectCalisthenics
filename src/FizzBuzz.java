7aff4d6fe1dbcd36a60f2ccae9923ee18be54745
jonay <casa@gmail.com>
2017-10-21 03:54:47 +0100

    refactor

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,6 +6,10 @@ class FizzBuzz {
     private List<WrapperFizzBuzz> list100;
 
     FizzBuzz() {
+        generateList();
+    }
+
+    private void generateList() {
         list100 = new ArrayList<>(101);
         list100.add(0, new WrapperFizzBuzz(new WrapperString("")));
         for(int i = 0; i < 100;i++){
